bca585f006d1aa6cba4b003d3c733cee7ad890bb
Claudiu Coman <fcoman2003@yahoo.com>
2013-09-28 12:16:27 +0200

    family config values for tiles

--- assets/scenes/family/scene.js
+++ assets/scenes/family/scene.js
@@ -1,23 +1,26 @@
 (function(scene) {
    
-   var tile = [];
+  var Config = scene.modules.ISR.Config;
+  // console.log(Config);
+  var tile = [];
+  
+  var skyHeight = 0; 
+  var tileWidth = Config.tile.width; 
+  var tileHeight = Config.tile.height; 
+  var tileLimitX = Config.tilesLimit.x; //12
+  var tileLimitY = Config.tilesLimit.y; 
+  
+  // for (var i = 0; i < 9; i++) {
+  for (var i = 0; i < tileLimitY; i++) {
+    for (var j = 0; j < (tileLimitX); j++) {
+      var style = 'top: ' + (skyHeight +(tileWidth * i)) + 'px; left: '+ (tileHeight * j) + 'px;background-color:rgb('+((i+j)*10)+','+((i+j)*15)+',100)';
 
-   var skyHeight = 0;
-   var screenWidth = 960;
-   var screenHeight = 720;
-   var tileWidth = 80;
-   var tileHeight = 80;
-
-   for (var i = 0; i < screenHeight / tileHeight; i++) {
-      for (var j = 0; j < (screenWidth / tileWidth); j++) {
-         var style = 'top: ' + (skyHeight + (tileWidth * i)) + 'px; left: ' + (tileHeight * j) + 'px;background-color:rgb(' + ((i + j) * 20) + ',' + ((i + j) * 15) + ',100)';
-
-         tile.push({
-            id : 'x' + i + '-y' + j,
-            style : style
-         });
-      }
-   }
+      tile.push({
+        id: 'x' + i + '-y' + j,
+        style: style
+      });
+    }
+  }
 
   scene.expose({
     title: scene.t('title', {

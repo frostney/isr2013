49f61f89baaca9eb1838f538a28c8adfad5aed41
Claudiu Coman <fcoman2003@yahoo.com>
2013-09-28 12:06:31 +0200

    church config values loaded for width and height

--- assets/scenes/church/scene.js
+++ assets/scenes/church/scene.js
@@ -10,17 +10,19 @@
 */
 
   console.log(scene.modules);
+  var Config = scene.modules.ISR.Config;
+  console.log(Config);
   var tile = [];
   
   var skyHeight = 0; 
-  var screenWidth = 960; 
-  var screenHeight = 720; 
-  var tileWidth = 80; 
-  var tileHeight = 80; 
+  var tileWidth = Config.tile.width; 
+  var tileHeight = Config.tile.height; 
+  var tileLimitX = Config.tilesLimit.x; //12
+  var tileLimitY = Config.tilesLimit.y; 
   
   // for (var i = 0; i < 9; i++) {
-  for (var i = 0; i < screenHeight/tileHeight; i++) {
-    for (var j = 0; j < (screenWidth/tileWidth); j++) {
+  for (var i = 0; i < tileLimitY; i++) {
+    for (var j = 0; j < (tileLimitX); j++) {
       var style = 'top: ' + (skyHeight +(tileWidth * i)) + 'px; left: '+ (tileHeight * j) + 'px;background-color:rgb('+((i+j)*10)+','+((i+j)*15)+',100)';
 
       tile.push({
